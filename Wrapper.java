37f0ebe806d9e320a4805c0f813fa124f178a369
alpshad <38167513+alpshad@users.noreply.github.com>
2021-11-04 17:59:20 -0700

    updated wrapper

--- Wrapper.java
+++ Wrapper.java
@@ -1,3 +1,26 @@
 public class Wrapper {
-    
+    private Type varType;
+    private String value;
+    private DynamicArray<Wrapper> array;
+
+    public Wrapper(Object value) {
+        // Check type
+        // Could come as "6", "Hello", "[1,2,3,4]"
+        //if Translator.isNumeric(value)
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof Wrapper)) {
+            return false;
+        }
+
+        Wrapper other = (Wrapper) o;
+
+        return (other.varType == this.varType) && (other.value.equals(this.value));
+    }
 }

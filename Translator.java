3e839d17fc5d14f35effcf4b0a8fe829d499be64
alpshad <38167513+alpshad@users.noreply.github.com>
2021-11-16 16:42:22 -0700

    Removed github annoyances
    Co-authored-by: eliacuna <eliacuna@users.noreply.github.com>

--- Translator.java
+++ Translator.java
@@ -162,11 +162,7 @@ public class Translator {
                 System.exit(1);
             }
 
-<<<<<<< HEAD
             handleCondFirst(condFirstMatch.group(1), pc);
-=======
-            handleCondFirst(condFirstMatch.group(1), scope);
->>>>>>> 1d34fbb1d0532ba042a0d53b5c33dc4e5f83ba95
         } else if (condElseMatch.find()) {
             // Conditional Else
             if (!condElseMatch.group(0).equals(instruction)) {
@@ -174,11 +170,7 @@ public class Translator {
                 System.exit(1);
             }
 
-<<<<<<< HEAD
             handleCondElse(condElseMatch.group(1), pc);
-=======
-            handleCondElse(condElseMatch.group(1), scope);
->>>>>>> 1d34fbb1d0532ba042a0d53b5c33dc4e5f83ba95
         } else if (endMatch.find()) {
             // End of block
             if (!endMatch.group(0).equals(instruction)) {
@@ -194,11 +186,7 @@ public class Translator {
                 System.exit(1);
             }
 
-<<<<<<< HEAD
             handleWhileFirst(whileFirstMatch.group(1), pc);
-=======
-            handleWhileFirst(whileFirstMatch.group(1), scope);
->>>>>>> 1d34fbb1d0532ba042a0d53b5c33dc4e5f83ba95
         } else if (forFirstMatch.find()) {
             // For loop header
             if (!forFirstMatch.group(0).equals(instruction)) {
@@ -206,11 +194,7 @@ public class Translator {
                 System.exit(1);
             }
 
-<<<<<<< HEAD
             handleForFirst(forFirstMatch.group(1), forFirstMatch.group(2), forFirstMatch.group(3), pc);
-=======
-            handleForFirst(forFirstMatch.group(1), forFirstMatch.group(2), forFirstMatch.group(3), scope);
->>>>>>> 1d34fbb1d0532ba042a0d53b5c33dc4e5f83ba95
         } else if (returnStatMatch.find()) {
             // Return statement
             if (!returnStatMatch.group(0).equals(instruction)) {
